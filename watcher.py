f467cf880b318f5547f722d81946de9bfcca1b4c
Fixsed <orioks.id@gmail.com>
2025-02-23 09:21:10 +0700

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+API_TOKEN = '7888331535:AAFZzdGeIjgZOhUXXj4aHKlZRHLBEVorv4Q'
+ADMIN_ID = '6990643296'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
